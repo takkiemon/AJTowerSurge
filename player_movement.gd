d16067ef8f5a1a31a0d0c45427f2be74cae99d38
takmanfa <takman@facilityapps.com>
2025-09-28 01:27:57 +0200

    add direction to the player capsule and thus a shooting direction; add fire rate to shooting; remove gravity;

--- player_movement.gd
+++ player_movement.gd
@@ -4,19 +4,23 @@ extends CharacterBody3D
 @export var speed = 14
 # The downward acceleration when in the air, in meters per second squared.
 @export var fall_acceleration = 75
-var bullet_scene = preload("res://test_scenes/test_bullet.tscn")
+@export var fire_rate = .2
+var firing_timer = 0
 
+@onready var bullet_scene = preload("res://test_scenes/test_bullet.tscn")
+@onready var camera = get_node("Camera3D")
+
+var forward = Vector3(1, 0, 0)
 var target_velocity = Vector3.ZERO
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+var rayOrigin = Vector3.ZERO
+var rayEnd = Vector3.ZERO
 
 func _physics_process(delta):
+	_move_player()
+	_look_at_mouse()
+	_shoot_bullet(delta)
+
+func _move_player():
 	# We create a local variable to store the input direction.
 	var direction = Vector3.ZERO
 
@@ -32,11 +36,6 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_up"):
 		direction.z -= 1
 		
-	if Input.is_action_pressed("shoot_test_bullet"):
-		var instance := bullet_scene.instantiate() as Node3D
-		instance.global_position = position
-		add_sibling(instance)
-		
 	if direction != Vector3.ZERO:
 		direction = direction.normalized()
 		# Setting the basis property will affect the rotation of the node.
@@ -46,10 +45,41 @@ func _physics_process(delta):
 		target_velocity.x = direction.x * speed
 		target_velocity.z = direction.z * speed
 
-		## Vertical Velocity
-		#if not is_on_floor(): # If in the air, fall towards the floor. Literally gravity
-			#target_velocity.y = target_velocity.y - (fall_acceleration * delta)
-
 		# Moving the Character
 		velocity = target_velocity
 		move_and_slide()
+	
+func _look_at_mouse():
+	#gettign the current phyisics state
+	var space_state = get_world_3d().direct_space_state
+	#getting the current mouse position
+	var mouse_position = get_viewport().get_mouse_position()
+
+	rayOrigin = camera.project_ray_origin(mouse_position)
+
+	rayEnd = rayOrigin + camera.project_ray_normal(mouse_position) * 2000
+	var params = PhysicsRayQueryParameters3D.new()
+	params.from = rayOrigin
+	params.to = rayEnd
+	var intersection = space_state.intersect_ray(params)
+
+	var pos = intersection.position
+	forward = Vector3(pos.x, 0, pos.z)
+	
+func _shoot_bullet(delta):
+	if Input.is_action_pressed("shoot_test_bullet"):
+		firing_timer += delta
+		if (firing_timer <= fire_rate):
+			return
+		var amount_of_bullets = firing_timer / fire_rate
+		firing_timer -= amount_of_bullets
+		for i in range(amount_of_bullets):
+			var instance := bullet_scene.instantiate() as Node3D
+			
+			instance.global_position = position
+			instance.set_shooting_variables(10, forward)
+			add_sibling(instance)
+	else:
+		firing_timer += delta
+		if (firing_timer > fire_rate):
+			firing_timer = fire_rate
--- test_scenes/test_bullet.gd
+++ test_scenes/test_bullet.gd
@@ -1,7 +1,12 @@
 extends Node3D
 
-@export var speed = 2
+@export var speed = 5
+var shooting_direction = Vector3(1, 0, 0)
 
 func _physics_process(delta):
-	var target_velocity = Vector3(1, 0, 0)
+	var target_velocity = shooting_direction.normalized() * speed
 	position += target_velocity * delta
+
+func set_shooting_variables(number, direction):
+	speed = number
+	shooting_direction = direction
